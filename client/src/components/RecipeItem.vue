21121c8248656408b1c2c7a2a173a76967d34e86
Elham <syedae@student.chalmers.se>
2020-09-28 18:29:28 +0200

    get all and create recipe

---
+++ client/src/components/RecipeItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>The {{recipe.name}} recipe takes {{recipe.duration}} min</p>
+        <b-button variant="danger" v-on:click="$emit('del-recipe', recipe._id)">delete</b-button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'recipe-item',
+  props: ['recipe'],
+  methods: {
+    // deleteRecipe() {
+    //   this.$emit('del-recipe', this.recipe._id)
+    //   console.log('del-recipe with id:' + this.recipe._id)
+    // }
+  }
+}
+</script>
+
+<style scoped>
+p {
+    background-color: aqua;
+}
+</style>
